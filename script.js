477a3c0bd72c15490109a47428556172b2f78120
Joaquin Touron <43842304+JayexDesigns@users.noreply.github.com>
2020-10-25 21:52:38 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+var mainText = document.getElementById("mainText");
+var num = 0;
+mainText.addEventListener("click", counter);
+
+function counter () {
+  num++;
+  if (num == 30) {
+    mainText.innerHTML = "<p>You Discovered<br>An Easter Egg</p>"
+    mainText.removeEventListener("click", counter);
+  }
+}
